2f507602b5c11612f47f9bf35b2524e3986646b9
Michael Ketzer <michael@ketzer.email>
2020-01-02 20:04:52 +0100

    Added service worker and next-offline;

---
+++ next.config.js
@@ -0,0 +1,28 @@
+const withOffline = require('next-offline');
+
+module.exports = withOffline({
+    target: "serverless",
+    transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
+  // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
+  // turn on the SW in dev mode so that we can actually test it
+  workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60,
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
+});
