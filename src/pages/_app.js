1470c5a1ce04cebd17041dfdac265ecc32cb602f
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-02-01 00:05:34 +0100

    Removed unrequired code;

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,12 +14,6 @@ const makeStore = () => {
 };
 
 class MyApp extends App {
-
-    static async getInitialProps({Component, ctx}) {
-        const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
-        return {pageProps};
-    }
-
     render() {
         const {Component, pageProps, store} = this.props;
         return (
