928f725d9163b1ecb47882ed7aa2acdb9f0cce64
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-02-01 00:05:34 +0100

    Removed unrequired code;

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,11 +1,9 @@
 import React, { ReactElement } from 'react';
 import PageFrame from '../components/PageFrame';
 import ArticleList from '../components/blocks/ArticleList';
-import Divider from '../components/Divider';
 
 export default function Home(): ReactElement {
   return <PageFrame>
-    <ArticleList />
-    <Divider />
+    <ArticleList />´
   </PageFrame>;
 }
