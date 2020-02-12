3a69e853b1023c51936d0b0f13efdb58d1718054
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-02-12 21:15:19 +0100

    Added redux store stuff for videos;

--- src/store/selectors/Ui.ts
+++ src/store/selectors/Ui.ts
@@ -3,3 +3,5 @@ import { State } from '../Store';
 export const availableArticlesSelector = (state: State) => state.ui.articles;
 export const loadedArticlesSelector = (state: State) => state.ui.loadedArticles;
 export const loadedAllTagsSelector = (state: State) => state.ui.loadedAllTags;
+export const availableVideosSelector = (state: State) => state.ui.videos;
+export const loadedVideosSelector = (state: State) => state.ui.loadedVideos;
