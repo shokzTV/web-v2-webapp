1f39f4456c0126f8929efed4e23afc769c8cd503
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-02-17 12:07:30 +0000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React, { ReactElement } from 'react'
 import PageFrame from '../Components/PageFrame';
-import LastNewsEntries from '../Components/blocks/LastNewsEntries';
+import LastNewsEntries from '../Components/Blocks/LastNewsEntries';
 
 export default function index(): ReactElement {
   return <>
