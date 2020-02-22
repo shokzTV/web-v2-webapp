2d8cccfd53760d60c132a29e3bb781afd66e133e
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-02-22 15:07:11 +0100

    Added video list;

--- src/pages/videos.tsx
+++ src/pages/videos.tsx
@@ -1,10 +1,11 @@
 import React, { ReactElement } from 'react'
 import PageFrame from '../components/PageFrame';
+import VideoList from '../components/blocks/VideoList';
 
 export default function Videos(): ReactElement {
   return <>
     <PageFrame showSelectedEvent={true}>
-
+        <VideoList />
     </PageFrame>
   </>;
 }
