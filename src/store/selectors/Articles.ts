45df9e34a189fb3f79ae7fc9d88d2887f937069a
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-02-23 09:56:33 +0100

    Started single article page;

--- src/store/selectors/Articles.ts
+++ src/store/selectors/Articles.ts
@@ -1,3 +1,10 @@
 import { State } from "../Store";
+import { createSelector } from "reselect";
+import memoize from "lodash/memoize";
 
 export const articlesSelector = (state: State) => state.entities.article || {}; 
+
+export const articleSelector = createSelector(
+    articlesSelector,
+    (articles) => memoize((id: number) => articles[id])
+)
