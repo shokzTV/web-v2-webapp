530e1b994ab9f88a60f10bf8d67545df5bec7430
Michael Ketzer <micha@Michaels-MacBook-Pro.local>
2020-02-24 19:23:13 +0100

    Article view;

---
+++ src/components/CKEditorContent.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement } from "react";
+import HtmlToReact  from 'html-to-react';
+import YouTube from 'react-youtube-embed';
+import { TwitterTweetEmbed } from 'react-twitter-embed';
+
+var processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions(React);
+var processingInstructions = [
+    {
+        shouldProcessNode: (node) => {
+            return node.name && node.name === 'oembed';
+        },
+        processNode: (node) =>  {
+            if(node.attribs && node.attribs.url.indexOf('youtube') !== -1) {
+                return <YouTube id={node.attribs.url} />;
+            }
+            if(node.attribs && node.attribs.url.indexOf('twitter') !== -1) {
+                const [, tweetId] = node.attribs.url.match(/^https:\/\/twitter\.com\/.*\/status\/(.*)$/);
+                return <TwitterTweetEmbed tweetId={tweetId} />;
+            }
+            console.error('Missing embedded support for ' + node.attribs.url)
+        },
+    },
+    {
+        shouldProcessNode: () => {
+            return true;
+        },
+        processNode: processNodeDefinitions.processDefaultNode,
+    },
+];
+  
+export default function CKEditorContent({text}: {text: string}): ReactElement {
+    return <>
+        {(new HtmlToReact.Parser()).parseWithInstructions(text, () => true, processingInstructions)}
+    </>;
+}
