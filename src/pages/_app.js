6578bda60f05c9b0c99d831a503fb02330821247
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-08 18:25:17 +0100

    Removed any stuff that could affect page size;

--- src/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import whyDidYouRender from '@welldone-software/why-did-you-render'
-
-if (typeof window !== 'undefined' && process.env.NODE_ENV === 'development') {
-  whyDidYouRender(React)
-}
-
-export default ({ Component, pageProps }) => <Component {...pageProps} />
