026fcd38332df304d202ced5814941cbba758240
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-11 23:07:23 +0100

    Added export path map;

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
 require('dotenv').config();
+const fetch = require('isomorphic-unfetch');
+
+async function get(url) {
+  const response = await fetch(process.env.API_URL + url);
+  return await response.json();
+}
+async function fetchArticleIds() {
+    return await get('/article/public/articleIds');
+}
+async function fetchFeaturedEvents() {
+  return await get('/event/featured');
+}
+async function fetchPastEventIds() {
+  return await get('/event/pastIds');
+}
+async function fetchAllEventIds() {
+  const featured = await fetchFeaturedEvents();
+  const ids = featured.map(({id}) => id);
+  const pastIds = await fetchPastEventIds();
+  return ids.concat(pastIds);
+}
 
 const withOffline = require('next-offline');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
@@ -6,6 +27,25 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 module.exports = withBundleAnalyzer(withOffline({
+  exportPathMap: async function() {
+    const paths = {
+      '/': { page: '/' },
+      '/articles': { page: '/articles' },
+      '/article/[articleId]': { page: '/article/[articleId]' },
+      '/dataProtection': { page: '/dataProtection' },
+      '/events': { page: '/events' },
+      '/event/[eventId]': { page: '/event/[eventId]' },
+      '/imprint': { page: '/imprint' },
+      '/videos': { page: '/videos' },
+    };
+    const articleIds = await fetchArticleIds();
+    const eventIds = await fetchAllEventIds();
+
+    articleIds.forEach(articleId => paths[`/article/${articleId}`] = { page: '/article/[articleId]' });
+    eventIds.forEach(eventId => paths[`/event/${eventId}`] = { page: '/event/[eventId]' });
+
+    return paths;
+  },
   target: 'serverless',
   transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
