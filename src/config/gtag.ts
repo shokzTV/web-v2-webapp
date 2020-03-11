93c5dcca4826b593762a2de38870d717373dca3a
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-11 20:12:22 +0100

    Added env variable for GA;

--- src/config/gtag.ts
+++ src/config/gtag.ts
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = 'UA-160269810-1';
+export const GA_TRACKING_ID = process.env.GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = url => {
