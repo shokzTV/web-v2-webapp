82713f5a7b44feb507297ea2c45089e159f3e45b
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-15 22:19:17 +0100

    Added static news export;

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -4,12 +4,23 @@ import NewsListHeader from '../components/pages/news/NewsListHeader';
 import Header from '../components/Header';
 import Divider from '../components/Divider';
 import NewsList from '../components/pages/news/NewsList';
+import { News } from '../api/@types/News';
+import { fetchAllNews } from '../api/news';
 
-export default function news(): ReactElement {
+export async function getStaticProps() {
+  const news = (await fetchAllNews()).sort(({id: a}, {id: b}) => b - a);
+  return {
+      props: {
+        news
+      }
+  };
+}
+
+export default function news({news}: {news: News[]}): ReactElement {
   return <PageFrame title={'News'}>
       <Header title={'Kurznachrichten'} topHeader />
       <Divider />
       <NewsListHeader />
-      <NewsList />
+      <NewsList news={news} />
   </PageFrame>;
 }
