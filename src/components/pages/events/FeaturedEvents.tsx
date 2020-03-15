7cea84110b54a460d3ab6c9e0c797495b181c9f4
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-15 22:29:45 +0100

    Added static export for events page;

--- src/components/pages/events/FeaturedEvents.tsx
+++ src/components/pages/events/FeaturedEvents.tsx
@@ -1,16 +1,14 @@
 import { ReactElement } from "react";
 import Header from "../../Header";
-import { loadFeaturedEvents } from "../../../hooks/featuredEvents";
 import Entry from "./FeaturedEvents/Entry";
+import { Event } from "../../../api/@types/Event";
 
-export default function FeaturedEvents(): ReactElement {
-    const list = loadFeaturedEvents();
-
+export default function FeaturedEvents({featuredEvents}: {featuredEvents: Event[]}): ReactElement {
     return <>
         <Header title={'Alle EVENTS'} link={'VORHERIGE EVENTS ANZEIGEN'} topHeader/>
 
         <div className={'eventsGrid'}>
-            {list.map((event, index) => <div key={event.id + '-' + index} className={'column'}><Entry event={event} /></div>)}
+            {featuredEvents.map((event, index) => <div key={event.id + '-' + index} className={'column'}><Entry event={event} /></div>)}
         </div>
 
         <style jsx>{`
--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -3,10 +3,26 @@ import PageFrame from '../components/PageFrame';
 import FeaturedEvents from '../components/pages/events/FeaturedEvents';
 import Divider from '../components/Divider';
 import PastEvents from '../components/pages/events/PastEvents';
+import { fetchFeaturedEvents } from '../api/event';
+import { Event } from '../api/@types/Event';
 
-export default function events(): ReactElement {
+interface Props {
+  featuredEvents: Event[];
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await fetchFeaturedEvents();
+  return {
+      props: {
+        featuredEvents,
+      }
+  };
+}
+
+
+export default function events({featuredEvents}: Props): ReactElement {
   return <PageFrame title={'Events'}>
-    <FeaturedEvents />
+    <FeaturedEvents featuredEvents={featuredEvents}/>
 
     <Divider double />
 
