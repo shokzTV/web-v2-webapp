46b8df422e36c5e4fa26cc87106a9e8a06bd8dcd
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-15 22:23:00 +0100

    Added tags to static exports;

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,9 +1,20 @@
 import React, { ReactElement } from 'react'
 import PageFrame from '../components/PageFrame';
 import TagList from '../components/pages/tags/TagList';
+import { Tag } from '../api/@types/Tag';
+import { fetchAllTags } from '../api/tag';
 
-export default function tags(): ReactElement {
+export async function getStaticProps() {
+  const tags = await fetchAllTags();
+  return {
+      props: {
+        tags
+      }
+  };
+}
+
+export default function tags({tags}: {tags: Tag[]}): ReactElement {
   return <PageFrame title={'Tags'}>
-    <TagList />
+    <TagList allTags={tags} />
   </PageFrame>;
 }
