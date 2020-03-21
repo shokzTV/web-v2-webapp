1e3bf4b63b98cbd389b2f962c840cb01e2e6ac25
Michael Ketzer <46134133+michaelketzer@users.noreply.github.com>
2020-03-21 07:25:46 +0100

    Typo

--- src/components/pages/events/FeaturedEvents.tsx
+++ src/components/pages/events/FeaturedEvents.tsx
@@ -5,7 +5,7 @@ import { Event } from "../../../api/@types/Event";
 
 export default function FeaturedEvents({featuredEvents}: {featuredEvents: Event[]}): ReactElement {
     return <>
-        <Header title={'Features Events'} link={'Weitere EVENTS ANZEIGEN'} topHeader linkTarget={'/events#weitere'} />
+        <Header title={'Featured Events'} link={'Weitere EVENTS ANZEIGEN'} topHeader linkTarget={'/events#weitere'} />
 
         <div className={'eventsGrid'}>
             {featuredEvents.map((event, index) => <div key={event.id + '-' + index} className={'column'}><Entry event={event} /></div>)}
